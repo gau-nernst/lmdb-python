6bea58b24240112f9be4ecd35b411049c4323d5c
Thien Tran <gau.nernst@yahoo.com.sg>
2022-05-30 20:03:00 +0800

    add tests for env copy and dbi delete

--- tests/test_c_api.py
+++ tests/test_c_api.py
@@ -81,6 +81,58 @@ def lmdb_env(tmp_path: pathlib.Path):
     return lmdb_c.LmdbEnvironment(str(tmp_path))
 
 
+def test_env_copy(lmdb_env, tmp_path):
+    copied_path = tmp_path / "new_folder"
+    os.makedirs(copied_path)
+    lmdb_env.copy(str(copied_path))
+    assert os.path.isfile(tmp_path / "data.mdb")
+    assert os.path.exists(tmp_path / "lock.mdb")
+
+
+def test_env_copy_fd(lmdb_env, tmp_path):
+    copied_path = tmp_path / "copied.mdb"
+    f = open(copied_path, "wb")
+    lmdb_env.copy_fd(f.fileno())
+    f.close()
+    lmdb_c.LmdbEnvironment(str(copied_path), read_only=True, no_subdir=True)
+
+
+def test_env_copy2(lmdb_env, tmp_path):
+    copied_path = tmp_path / "new_folder"
+    os.makedirs(copied_path)
+    lmdb_env.copy2(str(copied_path))
+    assert os.path.isfile(tmp_path / "data.mdb")
+    assert os.path.exists(tmp_path / "lock.mdb")
+
+
+def test_env_copy2_compact(lmdb_env, tmp_path):
+    copied_path = tmp_path / "new_folder"
+    os.makedirs(copied_path)
+    lmdb_env.copy2(str(copied_path), compact=True)
+    assert os.path.isfile(tmp_path / "data.mdb")
+    assert os.path.exists(tmp_path / "lock.mdb")
+    # TODO: add some data, remove some data,
+    # check the copied file is smaller
+
+
+def test_env_copy_fd2(lmdb_env, tmp_path):
+    copied_path = tmp_path / "copied.mdb"
+    f = open(copied_path, "wb")
+    lmdb_env.copy_fd2(f.fileno())
+    f.close()
+    lmdb_c.LmdbEnvironment(str(copied_path), read_only=True, no_subdir=True)
+
+
+def test_env_copy_fd2_compact(lmdb_env, tmp_path):
+    copied_path = tmp_path / "copied.mdb"
+    f = open(copied_path, "wb")
+    lmdb_env.copy_fd2(f.fileno(), compact=True)
+    f.close()
+    lmdb_c.LmdbEnvironment(str(copied_path), read_only=True, no_subdir=True)
+    # TODO: add some data, remove some data,
+    # check the copied file is smaller
+
+
 def test_env_get_stat(lmdb_env: lmdb_c.LmdbEnvironment):
     stats = lmdb_env.get_stat()
     assert isinstance(stats, lmdb_python.types.LmdbStat)
@@ -262,6 +314,60 @@ def make_dbi_with_data(make_txn: _MakeTxn):
         return dbi
 
     return _make_dbi_with_data
+
+
+def test_dbi_get_stat(make_txn: _MakeTxn):
+    txn = make_txn(read_only=False)
+    dbi = lmdb_c.LmdbDatabase(txn)
+    stat = dbi.get_stat(txn)
+    assert isinstance(stat, lmdb_python.types.LmdbStat)
+    for s in stat:
+        assert isinstance(s, int)
+
+
+def test_dbi_get_flags(make_txn: _MakeTxn):
+    txn = make_txn(read_only=False)
+    dbi = lmdb_c.LmdbDatabase(txn)
+    flags = dbi.get_flags(txn)
+    assert isinstance(flags, lmdb_python.types.LmdbDbFlags)
+    for f in flags:
+        assert isinstance(f, int)
+
+
+def test_dbi_empty_db(make_dbi_with_data: _MakeDbi, make_txn: _MakeTxn):
+    dbi = make_dbi_with_data([(b"key1", b"value1")])
+    txn = make_txn(read_only=True)
+    stat = dbi.get_stat(txn)
+    txn.abort()
+    assert stat.ms_entries == 1
+
+    txn = make_txn(read_only=False)
+    dbi.empty_db(txn)
+    txn.commit()
+
+    txn = make_txn(read_only=True)
+    stat = dbi.get_stat(txn)
+    txn.abort()
+    assert stat.ms_entries == 0
+
+
+def test_dbi_delete_db(tmp_path):
+    db_name = "database"
+    env = lmdb_c.LmdbEnvironment(str(tmp_path), max_dbs=1)
+    txn = lmdb_c.LmdbTransaction(env, read_only=False)
+    dbi = lmdb_c.LmdbDatabase(txn, name=db_name, create=True)
+    txn.commit()
+
+    txn = lmdb_c.LmdbTransaction(env, read_only=False)
+    dbi = lmdb_c.LmdbDatabase(txn, name=db_name)
+    dbi.delete_db(txn)
+    txn.commit()
+
+    txn = lmdb_c.LmdbTransaction(env, read_only=True)
+    with pytest.raises(lmdb_c.LmdbException) as e:
+        dbi = lmdb_c.LmdbDatabase(txn, name=db_name)
+    assert e.value.rc == lmdb_c.MDB_NOTFOUND
+    txn.abort()
 
 
 @pytest.mark.parametrize("key,value", _key_value_samples)
